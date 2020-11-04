a4e3f7df5ab706873bde1df613d92194f1da0c29
Divyateja Pasupuleti <divyateja2004@gmail.com>
2020-11-04 11:56:42 +0530

    edit

--- assets/javascript/full-page-scroll.js
+++ assets/javascript/full-page-scroll.js
@@ -248,3 +248,38 @@
 	};
 	window.fullScroll = fullScroll;
 })();
+
+
+
+
+
+
+// My Code
+function gettimefunc(tilldate,id){
+	// Update the count down every 1 second
+	var x =setInterval(function() {
+	  // Get todays date and time
+	  var now = new Date().getTime();
+	  // Find the distance between now an the count down date
+	  var tilldatecalc = new Date(tilldate).getTime();
+
+	  var distance = tilldatecalc - now;
+
+	  console.log(tilldatecalc);
+	  // Time calculations for days, hours, minutes and seconds
+	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	  // Display the result in the element with id="demo"
+	  document.getElementById(id).textContent = "This Opens in " + days + " Days " + hours + " Hours "
+	  + minutes + " Minutes " + seconds + " Seconds ";
+	  // If the count down is finished, write some text 
+	  if (distance < 0) {
+	  	clearInterval(x);
+	    document.getElementById(id).textContent = "";
+	  }
+	}, 1000);
+}
+
+var countdown = gettimefunc("Sep 19,2021 00:00:00","texttimed");//Change Date in the format Month Date , Year HH:MM:SS
